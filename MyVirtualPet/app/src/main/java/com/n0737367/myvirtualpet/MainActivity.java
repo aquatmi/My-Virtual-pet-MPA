03602c2e1ca75aeb9016d3add2e0ec5e0dd31288
aquatmi <61705188+aquatmi@users.noreply.github.com>
2020-03-18 13:33:13 +0000

    3rd times the charm

---
+++ MyVirtualPet/app/src/main/java/com/n0737367/myvirtualpet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.n0737367.myvirtualpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
