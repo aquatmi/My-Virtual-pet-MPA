61969b1e0788b11059280519c211e068f0d94822
aquatmi <61705188+aquatmi@users.noreply.github.com>
2020-03-18 14:22:46 +0000

    Working Tabs/Fragments

--- MyVirtualPet/app/src/androidTest/java/com/n0737367/myvirtualpet/ExampleInstrumentedTest.java
+++ MyVirtualPet/app/src/androidTest/java/com/n0737367/myvirtualpet/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.n0737367.myvirtualpet;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- MyVirtualPet/app/src/main/java/com/n0737367/myvirtualpet/MainActivity.java
+++ MyVirtualPet/app/src/main/java/com/n0737367/myvirtualpet/MainActivity.java
@@ -1,13 +1,58 @@
 package com.n0737367.myvirtualpet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem petTab, mapTab, OptionsTab;
+    public PageAdapter pageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout)findViewById(R.id.TabLayout);
+        petTab = (TabItem)findViewById(R.id.PetTab);
+        mapTab = (TabItem)findViewById(R.id.MapTab);
+        OptionsTab = (TabItem)findViewById(R.id.OptionsTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition() == 0) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 1) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 2) {
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
